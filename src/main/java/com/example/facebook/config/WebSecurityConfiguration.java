6dcf48dbc73480ade7061cdf305c295af5661319
Ptr <petarman96@abv.bg>
2020-07-02 00:16:28 +0300

    Added authentication

--- src/main/java/com/example/facebook/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/facebook/config/WebSecurityConfiguration.java
@@ -1,4 +1,42 @@
 package com.example.facebook.config;
 
-public class WebSecurityConfiguration {
+import com.example.facebook.service.implementation.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public WebSecurityConfiguration(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @Override
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/register").permitAll()
+                .anyRequest().authenticated()
+                    .and()
+                .formLogin().loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/profile.html", true).permitAll()
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                .and()
+                    .logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                    .exceptionHandling().accessDeniedPage("/unauthorized")
+                .and()
+                    .csrf().disable();
+
+    }
 }
